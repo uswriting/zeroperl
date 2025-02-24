0a45fedfadacaf232b02079661c6fd3e2f061981
andrew <1297077+andrewmd5@users.noreply.github.com>
2025-02-24 19:52:21 +0900

    Update zeroperl.c

--- stubs/zeroperl.c
+++ stubs/zeroperl.c
@@ -575,7 +575,12 @@ ssize_t __wrap_read(int fd, void *buf, size_t count) {
         // SFS was successful, just return
         return r;
     }
+    
+    
+    
      DEBUG_LOG("real c");
+     asyncify_start_unwind(16);
+
      r = __real_read(fd, buf, count);
      DEBUG_LOG("back from real c");
 
