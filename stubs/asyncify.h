3f30d95c7a5c1533af6e0d0f3027310ecf9a05bf
andrew <1297077+andrewmd5@users.noreply.github.com>
2025-02-24 13:59:19 +0900

    Update asyncify.h

--- stubs/asyncify.h
+++ stubs/asyncify.h
@@ -23,6 +23,8 @@ __attribute__((import_module("asyncify"), import_name("start_rewind"))) void asy
 
 __attribute__((import_module("asyncify"), import_name("stop_rewind"))) void asyncify_stop_rewind(void);
 
-extern int asyncify_get_state(void);
+__attribute__((import_module("asyncify"), import_name("get_state"))) int __asyncify_get_state(void);
+
+#define asyncify_get_state() __asyncify_get_state()
 
 #endif
